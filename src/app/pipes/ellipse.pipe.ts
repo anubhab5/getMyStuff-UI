a05a1246dbda553c5f4d0b0cca0155ef8667b842
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-04-16 17:43:30 +0530

    Eliipsis task

--- src/app/pipes/ellipse.pipe.ts
+++ src/app/pipes/ellipse.pipe.ts
@@ -5,6 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EllipsePipe implements PipeTransform {
 
+  /*
+  The pipe will be taking a string value and add ellipsis to the value after certain user provided characters
+  eg-
+  <p>{{description | EllipsePipe : 45}}</p>
+  here if the description is more than 45 characters then eliipsis will be put after 45th character else not
+  
+  */
   transform(value: unknown, ...args: unknown[]): unknown {
     return null;
   }
